1827189a9b839d377885e8b9d48fafbd28790cb3
Danilo <danilo.santitto@gmail.com>
2020-04-30 21:08:52 +0200

    Updates

---
+++ precache-manifest.119c793922cb7bd7fafd0ef2ab1d2651.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd91ac33a1aa3c1f26fc23871b67f82",
+    "url": "/Mappa-di-Karnaugh/index.html"
+  },
+  {
+    "revision": "57ad10b8c1d645c0d8f8",
+    "url": "/Mappa-di-Karnaugh/static/css/main.a5e43b1c.chunk.css"
+  },
+  {
+    "revision": "07d69f4be9c30b66f301",
+    "url": "/Mappa-di-Karnaugh/static/js/2.4e013504.chunk.js"
+  },
+  {
+    "revision": "6e742f513d9938d07b2d93218f3e4449",
+    "url": "/Mappa-di-Karnaugh/static/js/2.4e013504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57ad10b8c1d645c0d8f8",
+    "url": "/Mappa-di-Karnaugh/static/js/main.33f9494b.chunk.js"
+  },
+  {
+    "revision": "75f313603e61779cb193",
+    "url": "/Mappa-di-Karnaugh/static/js/runtime-main.83d1f6f2.js"
+  }
+]);
